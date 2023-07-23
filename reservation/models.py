0293b7902e699c46d1e562bfaad77d4a70fa96dc
ShmuelAmir <shmulik5777@gmail.com>
2023-07-23 12:58:35 +0300

    add models

--- reservation/models.py
+++ reservation/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
+
 # Create your models here.
+class menu(models.Model):
+    ID = models.IntegerField(primary_key=True, max_length=5)
+    Title = models.CharField(max_length=255)
+    Price = models.DecimalField(max_digits=10, decimal_places=2)
+    Inventory = models.IntegerField(max_length=5)
+
+    def __str__(self):
+        return self.Title
+
+
+class booking(models.Model):
+    ID = models.IntegerField(primary_key=True, max_length=11)
+    Name = models.CharField(max_length=255)
+    No_of_guests = models.IntegerField(max_length=6)
+    BookingDate = models.DateField()
+
+    def __str__(self):
+        return self.Name
